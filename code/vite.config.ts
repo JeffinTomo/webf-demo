1c9115dbb07f39720e4deb1d4db1420ff6e353e5
水色 <xuezhengfeng@tomo.inc>
2025-12-29 11:30:28 +0800

    feat: pre build-

--- code/vite.config.ts
+++ code/vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     preserveSymlinks: true,
-    dedupe: ['@wlfi/webf-point'],
+    alias: {
+      '@wlfi/webf-point': path.resolve(__dirname, 'webf-modules/webf-point/dist/index.mjs'),
+    },
   },
   optimizeDeps: {
     exclude: ['@wlfi/webf-point'],
